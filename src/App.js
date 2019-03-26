cde080f9f220a753ebf220e3b611e4a2f8daaf64
Roland Askew <rolandjamesaskew37@gmail.com>
2019-03-26 19:29:46 +1300

    completed preliminary add ingredient modal, for the Recipe Detail.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ class App extends Component {
     this.handleAddRecipe = this.handleAddRecipe.bind(this);
     this.handleRemoveRecipe = this.handleRemoveRecipe.bind(this);
     this.handleUpdateRecipe = this.handleUpdateRecipe.bind(this);
+    this.addIngredient = this.addIngredient.bind(this);
+    this.hasIngredient = this.hasIngredient.bind(this);
 
     this.state = {
       products: [
@@ -144,12 +146,38 @@ class App extends Component {
     this.setState({recipes: recipes,});
   }
 
+  hasIngredient(ingredient) {
+    let ingredients = this.state.products.slice();
+    const fields = ["name", "coo", "brand", "category", "subcategory"];
+    fields.forEach( field => {
+      ingredients = ingredients.filter( item => {
+        return item[field] === ingredient[field];
+      });
+    })
+
+    if (ingredients.length === 0) {
+      return -1;
+    }
+
+    return ingredients[0].id;
+  }
+
+  addIngredient(ingredient) {
+    const products = this.state.products.slice();
+    let maxId = Math.max.apply(Math, products.map(x => x.id));
+    ingredient.id = maxId + 1;
+    products.push(ingredient);
+    this.setState({products: products});
+    return ingredient.id;
+  }
+
   render() {
     return (
       <div className="App">
         <RecipesView recipes={this.state.recipes} products={this.state.products} 
           handleAddRecipe={this.handleAddRecipe} handleRemoveRecipe={this.handleRemoveRecipe}
-          handleUpdateRecipe={this.handleUpdateRecipe} />
+          handleUpdateRecipe={this.handleUpdateRecipe} hasIngredient={this.hasIngredient}
+          handleAddNewIngredient={this.addIngredient} />
       </div>
     );
   }
--- src/recipes.js
+++ src/recipes.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './recipes.css';
-import {FaPencilAlt, FaTrash, FaPlusSquare, FaSave, FaBan} from 'react-icons/fa';
+import {IconContext} from 'react-icons';
+import {FaPencilAlt, FaTrash, FaPlusSquare, FaSave, FaRegTimesCircle} from 'react-icons/fa';
 import PropTypes from 'prop-types';
 
 class RecipesView extends Component {
@@ -54,10 +55,12 @@ class RecipesView extends Component {
             <div className="recipes-view">
                 <RecipeMaster recipes={this.props.recipes} 
                   currentRecipe={this.state.selectedRecipeId} 
-                  handleAddRecipe={this.props.handleAddRecipe} 
+                  handleAddRecipe={this.props.handleAddRecipe}
                   handleSelectRecipe={this.handleSelectRecipe}
                   handleRemoveRecipe={this.handleRemoveRecipe} />
-                <RecipeDetail recipe={recipe} handleUpdateRecipe={this.props.handleUpdateRecipe}/>
+                <RecipeDetail recipe={recipe} handleUpdateRecipe={this.props.handleUpdateRecipe}
+                handleAddNewIngredient={this.props.handleAddNewIngredient}
+                hasIngredient={this.props.hasIngredient} />
             </div>            
         );
     }
@@ -129,6 +132,8 @@ RecipeMaster.propTypes = {
     currentRecipe: PropTypes.number.isRequired,
     recipes: PropTypes.array.isRequired,
     handleAddRecipe: PropTypes.func.isRequired,
+    handleSelectRecipe: PropTypes.func.isRequired,
+    handleRemoveRecipe: PropTypes.func.isRequired,
 }
 
 class RecipeDetail extends Component {
@@ -147,6 +152,17 @@ class RecipeDetail extends Component {
             newInstruction: "",
             editingInstruction: -1,
             editedInstructionText: "",
+            showNewIngredientModal: false,
+            newIngredient: {
+                name: "",
+                category: "",
+                subcategory: "",
+                coo: "",
+                brand: "",
+                defaultunit: "",
+            },
+            newIngredientQuantity: 0,
+            newIngredientUnit: "",
         }
 
         this.handleSubmitNewInstruction = this.handleSubmitNewInstruction.bind(this);
@@ -157,6 +173,13 @@ class RecipeDetail extends Component {
         this.handleToggleEditingInstruction = this.handleToggleEditingInstruction.bind(this);
         this.handleCancelEditingInstruction = this.handleCancelEditingInstruction.bind(this);
         this.handleSaveEditedInstruction = this.handleSaveEditedInstruction.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleAddIngredientChange = this.handleAddIngredientChange.bind(this);
+        this.handleSubmitIngredient = this.handleSubmitIngredient.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({showNewIngredientModal: !this.state.showNewIngredientModal});
     }
 
     handleCancelEditingInstruction() {
@@ -214,6 +237,38 @@ class RecipeDetail extends Component {
         this.setState({[name]: value});
     }
 
+    handleAddIngredientChange(event) {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+        const newIngredient = this.state.newIngredient;
+        newIngredient[name] = value;
+        this.setState({newIngredient: newIngredient});
+    }
+
+    handleSubmitIngredient(event) {
+        event.preventDefault();
+        this.setState({showNewIngredientModal: !this.state.showNewIngredientModal});
+        const newIngredient = this.state.newIngredient;
+        let id;
+        id = this.props.hasIngredient(newIngredient);
+        if(id < 0) {
+            id = this.props.handleAddNewIngredient(newIngredient);
+        }
+
+        const recipe = this.props.recipe;
+        const ingredients = recipe.ingredients.slice();
+        const recipeIngredient = {
+            id: id, 
+            quantity: this.state.newIngredientQuantity,
+            unit: this.state.newIngredientUnit,
+        }
+
+        ingredients.push(recipeIngredient);
+        recipe.ingredients = ingredients;
+        this.props.handleUpdateRecipe(recipe);
+    }
+
     render() {
         let title;
         let ingredients;
@@ -236,21 +291,19 @@ class RecipeDetail extends Component {
                     <li key={step}>
                         {
                             this.state.editingInstruction === step ?
-                            <React.Fragment>
-                                <form className="detail-instruction" onSubmit={(e) => this.handleSaveEditedInstruction(e, step)}>
-                                    <input value={this.state.editedInstructionText} onChange={this.handleFormChange}
-                                        name="editedInstructionText" required placeholder="There must be an instruction here." />
-                                    <button type="submit"
-                                        ><FaSave/></button>
-                                    <button type="button" 
-                                        onClick={this.handleCancelEditingInstruction}><FaBan/></button>
-                                </form>
-                            </React.Fragment> :
-                            <React.Fragment>
-                                <div className="content">{step + 1}. {item}</div>
+                            <form className="detail-instruction" onSubmit={(e) => this.handleSaveEditedInstruction(e, step)}>
+                                <input value={this.state.editedInstructionText} onChange={this.handleFormChange}
+                                    name="editedInstructionText" required placeholder="There must be an instruction here." />
+                                <button type="submit"
+                                    ><FaSave/></button>
+                                <button type="button" 
+                                    onClick={this.handleCancelEditingInstruction}><FaRegTimesCircle/></button>
+                            </form> :
+                            <div className="detail-instruction">
+                                <div className="content">{item}</div>
                                 <button type="button" onClick={() => this.handleToggleEditingInstruction(step)}><FaPencilAlt/></button>
                                 <button type="button" onClick={() => this.handleRemoveInstruction(step)}><FaTrash/></button>
-                            </React.Fragment>
+                            </div>
                         }
                     </li>
                 )
@@ -273,7 +326,7 @@ class RecipeDetail extends Component {
                                 <button type="submit"
                                     ><FaSave/></button>
                                 <button type="button" 
-                                    onClick={this.handleToggleTitleForm}><FaBan/></button>
+                                    onClick={this.handleToggleTitleForm}><FaRegTimesCircle/></button>
                             </form>
                         }
 
@@ -282,7 +335,7 @@ class RecipeDetail extends Component {
                             <ul>
                                 {ingredients}
                             </ul>
-                            <button type="button">Add Ingredient...</button>
+                            <button type="button" onClick={this.toggleModal}>Add Ingredient...</button>
                         </div>
 
                         <div className="detail-instructions">
@@ -304,10 +357,136 @@ class RecipeDetail extends Component {
                         <p>No recipe is selected.</p>
                     </div>
                 }
-                
+                <Modal show={this.state.showNewIngredientModal} onClose={(e) => this.toggleModal()}>
+                    <section className="add-ingredient">
+                        <form onSubmit={this.handleSubmitIngredient}>
+                            <div className="title">Add Ingredient</div>
+                            <div className="row">
+                                <label className="col-1">Name:</label>
+                                <input className="col-2" required name="name"
+                                  placeholder="name of ingredient" 
+                                  list="names"
+                                  onChange={this.handleAddIngredientChange}
+                                  value={this.state.newIngredient.name} />
+                                <datalist id="names">
+                                </datalist>
+                            </div>
+                            <div className="row">
+                                <label className="col-1">Category:</label>
+                                <input className="col-2" required 
+                                  name="category" placeholder="category" 
+                                  list="categories"
+                                  onChange={this.handleAddIngredientChange}
+                                  value={this.state.newIngredient.category}/>
+                                <datalist id="categories">
+                                </datalist>
+                                <label className="col-4">Subcategory:</label>
+                                <input className="col-5" required 
+                                  name="subcategory" placeholder="subcategory" 
+                                  list="subcategories"
+                                  onChange={this.handleAddIngredientChange}
+                                  value={this.state.newIngredient.subcategory}/>
+                                <datalist id="subcategories">
+                                </datalist>
+                            </div>
+                            <div className="row">
+                                <input className="col-2" 
+                                  required name="brand" placeholder="brand" 
+                                  list="brands"
+                                  onChange={this.handleAddIngredientChange}
+                                  value={this.state.newIngredient.brand}/>
+                                <datalist id="brands">
+                                </datalist>
+                                <input className="col-5" 
+                                  required name="coo" placeholder="country of origin" 
+                                  list="coo-sources"
+                                  onChange={this.handleAddIngredientChange}
+                                  value={this.state.newIngredient.coo}/>
+                                <datalist id="coo-sources">
+                                </datalist>
+                            </div>
+                            <div className="row"></div>
+                            <div className="row"></div>
+                            <div className="row">
+                                <input className="col-5" 
+                                  required name="defaultunit" placeholder="default unit" 
+                                  list="unit-defaults"
+                                  onChange={this.handleAddIngredientChange}
+                                  value={this.state.newIngredient.defaultunit}/>
+                                <datalist id="unit-defaults">
+                                </datalist>
+                            </div>
+                            <div className="row"></div>
+                            <div className="row">
+                                <label className="col-1" >Quantity:</label>
+                                <input className="col-2" type="number" 
+                                  min="0" required 
+                                  name="newIngredientQuantity" 
+                                  onChange={this.handleFormChange}
+                                  value={this.state.newIngredientQuantity}/>
+                                <label className="col-4" >Unit:</label>
+                                <input className="col-5" required
+                                  name="newIngredientUnit" 
+                                  onChange={this.handleFormChange}
+                                  value={this.state.newIngredientUnit}/>
+                            </div>
+                            <div className="button-pane">
+                                <button type="button" onClick={this.toggleModal}>
+                                <IconContext.Provider value={{size: "1.1em"}}>
+                                    <FaRegTimesCircle/>
+                                </IconContext.Provider>
+                                </button>
+                                <button type="submit">
+                                <IconContext.Provider value={{size: "1.1em"}}>
+                                    <FaSave/>
+                                </IconContext.Provider>
+                                </button>
+                            </div>
+                        </form>
+                    </section>
+                </Modal>
             </section>
         );
     }
+}
+
+RecipeDetail.propTypes = {
+    handleUpdateRecipe: PropTypes.func.isRequired,
+    hasIngredient: PropTypes.func.isRequired,
+    handleAddNewIngredient: PropTypes.func.isRequired,
+    recipe: PropTypes.object,
+}
+
+class Modal extends Component {
+    render() {
+        return (
+            <React.Fragment>
+            {
+                !this.props.show ?
+                "" :
+                <div className="modal-background">
+                    <button type="button" 
+                      className="modal-button-close" 
+                      onClick={this.props.onClose}>
+                        <IconContext.Provider value={{color: "white", size: "3em"}}>
+                            <FaRegTimesCircle/>
+                        </IconContext.Provider>
+                    </button>
+                    <div className="modal">
+                      {this.props.children}
+                    </div>
+                    <div className="modal-shadow">
+                    </div>
+                </div>
+            }
+            </React.Fragment>
+        )
+    }
+}
+
+Modal.propTypes = {
+    onClose: PropTypes.func,
+    show: PropTypes.bool,
 }
 
 export default RecipesView;
